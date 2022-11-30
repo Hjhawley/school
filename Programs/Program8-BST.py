a2191cfb00bb5e8413df65330328bfdd16baa5ae
Hayden Hawley <hjhawley7@gmail.com>
2022-11-29 17:33:39 -0700

    Program 8

--- Programs/Program8-BST.py
+++ Programs/Program8-BST.py
@@ -10,4 +10,89 @@ After you get it working with the regular lists, try it on the Medium sized list
 In this case, do not print every student that failed to insert, delete, or retrieve. Just print the total fails in each category.
 '''
 
-# unordered unique students
+# Modify this
+
+import time
+from student import Student
+from bst import BST
+
+def callbackFunction(s):
+    global ageTotal
+    ageTotal += int(s.mAge)
+
+def main():
+    # Insert
+    start = time.time()
+    f = open("FakeNames.txt")
+    allStudents = BST()
+    for line in f:
+        fields = line.split()
+        s = Student(fields[0], fields[1], fields[2], fields[3], fields[4])
+        if allStudents.insert(s) == False:
+            print("Error: Duplicate SSN for " + s.mFirstName + " " + s.mLastName)
+    f.close()
+    end = time.time()
+    print("Time for insert: " + str(end - start) + " seconds")
+    
+    # Traverse
+    start = time.time()
+    averageAge = 0
+    count = allStudents.size()
+    allStudents.traverse(callbackFunction)
+    averageAge = ageTotal/count
+    print("Average age: " + str(averageAge))
+    end = time.time()
+    print("Time for traverse: " + str(end - start) + " seconds")
+    
+    # Delete
+    start = time.time()
+    f = open("DeleteNames.txt")
+    for line in f:
+        SSN = line.strip()
+        s2 = Student("", "", SSN, "", "") # Dummy student
+        if allStudents.delete(s2) == False:
+            print("Error: SSN " + SSN + " does not exist.")
+    f.close()
+    end = time.time()
+    print("Time for delete: " + str(end - start) + " seconds")
+    
+    # Retrieve
+    start = time.time()
+    f = open("RetrieveNames.txt")
+    rTotal = 0
+    rAge = 0
+    for line in f:
+        SSN = line.strip()
+        s2 = Student("", "", SSN, "", "")
+        retrieved = allStudents.retrieve(s2)
+        if retrieved == None:
+            print("Error: SSN " + SSN + " does not exist.")
+        else:
+            rTotal += 1
+            rAge += int(retrieved.mItem.mAge)
+    if rTotal == 0:
+        print("Error: retrieve count == 0")
+    else:
+        averageAge = rAge/rTotal
+        print("Average age: " + str(averageAge))
+    f.close()
+    end = time.time()
+    print("Time for retrieve: " + str(end - start) + " seconds")
+
+main()
+
+'''
+Program 5 Comparison
+
+Error: Duplicate SSN for DAMON POTTS
+Time for insert: 36.41500735282898 seconds
+Average age: 42.92993099769993
+Time for traverse: 0.020928382873535156 seconds
+Error: SSN 010-16-5079 does not exist.
+Time for delete: 45.17820739746094 seconds
+Average age: 49.537614132592296
+Time for retrieve: 2.071270704269409 seconds
+
+Program 6 Comparison
+
+'''
--- Programs/bst.py
+++ Programs/bst.py
@@ -3,9 +3,9 @@
 from node import Node
 
 class BST:
-    
+
     def __init__(self):
-        pass
+        self.mRoot = None
 
     def insert(self, item):
         if self.exists(item):
@@ -17,5 +17,56 @@ class BST:
     def insertR(self, n, current):
         if current is None:
             current = n
-        elif n.mItem < current.mItem:
+        elif n.mItem < current.mItem: # Walk down the tree
+            current.mL = self.insertR(n, current.mL)
+        else:
             current.mR = self.insertR(n, current.mR)
+    
+    def retrieve(self, item):
+        return retrieveR(item, self.mRoot)
+    
+    def retrieveR(self, item, current):
+        if current is None:
+            return None
+        elif item == current.mItem:
+            return current.mItem
+        elif item < current.mItem:
+            return self.retrieveR(item, current.mL)
+        else:
+            return self.retrieveR(item, current.mR)
+    
+    def size(self):
+        return self.sizeR(self.mRoot)
+    
+    def sizeR(self, current):
+        if current is None:
+            return 0
+        count = 1
+        count += sizeR(current.mL)
+        count += sizeR(current.mR)
+        return count
+    
+    def traverse(self, callback, data):
+        current = self.mRoot
+        self.traverseR(callback, data, current)
+    
+    def traverseR(self, callback, data, current):
+        if current is None: # == ?
+            return
+        callback(current.mItem.data)
+        self.traverseR(callback, data, current.mL)
+        self.traverseR(callback, data, current.mR)
+    
+    def exists(self, item):
+        exist = self.existsR(item, self.mRoot)
+        return exist
+    
+    def existsR(self, n, current):
+        if current is None:
+            return False
+        elif current.mItem == nItem:
+            return True
+        elif n.mItem < current.mItem:
+            return self.existsR(n, current.mL)
+        else:
+            return self.existsR(n, current.mR)
