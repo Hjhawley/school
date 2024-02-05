585102d353f558aa39fea43d14fbb40f4488058c
Hayden <hjhawley7@gmail.com>
2024-02-05 10:16:18 -0700

    Restructure

---
+++ 24-Spring/CS3600 Graphics Programming/Assignments/2-BouncingCircles2/circle.js
@@ -0,0 +1,98 @@
+class Circle{
+    constructor(xlow, xhigh, ylow, yhigh){ // make the circles inside these World Coordinates
+        this.xlow = xlow;
+        this.xhigh = xhigh;
+        this.ylow = ylow;
+        this.yhigh = yhigh;
+        this.color = [Math.random(), Math.random(), Math.random(), 1]
+        this.size = 1.0 + Math.random(); // half edge between 1.0 and 2.0
+        const minx = xlow+this.size;
+        const maxx = xhigh-this.size;
+        this.x = minx + Math.random()*(maxx-minx);
+        const miny = ylow+this.size;
+        const maxy = yhigh-this.size;
+        this.y = miny + Math.random()*(maxy-miny);
+        this.degrees = Math.random()*90;
+        this.dx = Math.random()*19+1; // 1 to 20
+        if (Math.random()>.5)
+            this.dx = -this.dx;
+        this.dy = Math.random()*19+1; // 1 to 20
+        if (Math.random()>.5)
+            this.dy = - this.dy;
+        this.sides = 64;
+    }
+    update(DT){
+        const degreesPerSecond = 45;
+        this.degrees += degreesPerSecond*DT;
+        this.degrees = 0.0; // turn off the rotation, for now
+
+        if(this.x+this.dx*DT +this.size > this.xhigh){
+            this.dx = -Math.abs(this.dx);
+        }
+        if(this.x+this.dx*DT -this.size < this.xlow){
+            this.dx = Math.abs(this.dx);
+        }
+        if(this.y+this.dy*DT +this.size > this.yhigh){
+            this.dy = -Math.abs(this.dy);
+        }
+        if(this.y+this.dy*DT -this.size < this.ylow){
+            this.dy = Math.abs(this.dy);
+        }
+
+
+        this.x += this.dx*DT;
+        this.y += this.dy*DT;
+    }
+    draw(gl, shaderProgram){
+        drawCircle(gl, shaderProgram, this.color, this.degrees, this.x, this.y, this.size, this.sides);
+    }    
+}
+
+function drawCircle(gl, shaderProgram, color, degrees, x, y, size, sides) {
+    const vertices = [];
+    for (let i = 0; i <= sides; i++) {
+        const angle = 2 * Math.PI * i / sides;
+        vertices.push(Math.cos(angle), Math.sin(angle));
+    }
+
+	const vertexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBufferObject);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+	//
+	// Set Vertex Attributes
+	//
+	const positionAttribLocation = gl.getAttribLocation(shaderProgram, 'vertPosition');
+	gl.vertexAttribPointer(
+		positionAttribLocation, // Attribute location
+		2, // Number of elements per attribute
+		gl.FLOAT, // Type of elements
+		gl.FALSE,
+		2 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		0 // Offset from the beginning of a single vertex to this attribute
+	);
+	gl.enableVertexAttribArray(positionAttribLocation);
+
+	//
+	// Set Uniform uColor
+	//
+	const colorUniformLocation = gl.getUniformLocation(shaderProgram, "uColor");
+	gl.uniform4fv(colorUniformLocation, color);
+
+	//
+	// Set Uniform uModelViewMatrix
+	//
+    const modelViewMatrixUniformLocation = gl.getUniformLocation(shaderProgram, "uModelViewMatrix");
+    const modelViewMatrix = mat4.create();
+    mat4.translate(modelViewMatrix, modelViewMatrix, [x, y, 0]);
+    mat4.scale(modelViewMatrix, modelViewMatrix, [size, size, 1]);
+    mat4.rotate(modelViewMatrix, modelViewMatrix, (degrees* Math.PI / 180), [0, 0, 1]);
+    gl.uniformMatrix4fv( modelViewMatrixUniformLocation, false, modelViewMatrix);	  	
+
+    //
+    // Starts the Shader Program, which draws the current object to the screen.
+    //
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, sides + 1);
+}
+
+export { Circle, drawCircle };
