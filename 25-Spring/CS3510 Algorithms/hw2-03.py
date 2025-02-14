5cb47ceec0724476191ff1c39e464643ac1fd1f2
Hayden <hjhawley7@gmail.com>
2025-02-13 22:39:02 -0700

    algos HW 2

--- 25-Spring/CS3510 Algorithms/hw2-03.py
+++ 25-Spring/CS3510 Algorithms/hw2-03.py
@@ -106,7 +106,7 @@ def reach_target_volume(size_A, size_B, target_vol):
 
 def main():
     # Example 1: 5-gallon and 3-gallon jugs with target 4 gallons.
-    print("Example 1: size_A=6, size_B=4, taget_vol=1")
+    print("Example 1: size_A=6, size_B=4, target_vol=1")
     result = reach_target_volume(6, 4, 1)
     if result == "unreachable":
         print("unreachable")
@@ -116,7 +116,7 @@ def main():
     print()
 
     # Example 2: 7-gallon and 5-gallon jugs with target 6 gallons.
-    print("Example 2: size_A=11, size_B=5, taget_vol=8")
+    print("Example 2: size_A=11, size_B=5, target_vol=8")
     result = reach_target_volume(11, 5, 8)
     if result == "unreachable":
         print("unreachable")
