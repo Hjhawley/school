42287b374483d857c77004af42a2f5129ab5b385
Hayden <hjhawley7@gmail.com>
2025-02-28 13:49:24 -0700

    more thorough tests

--- 25-Spring/CS4550 Compilers/compiler/Main.cpp
+++ 25-Spring/CS4550 Compilers/compiler/Main.cpp
@@ -6,7 +6,6 @@
 
 void testScanner() {
     std::cout << "----- Scanner Test -----\n";
-
     const std::string inputFileName = "code.txt";
     ScannerClass scanner(inputFileName);
     TokenType tt;
@@ -25,21 +24,17 @@ void testScanner() {
 
 void testSymbolTable() {
     std::cout << "\n----- Symbol Table Test -----\n";
-
     SymbolTableClass symbolTable;
     symbolTable.AddEntry("x");
     symbolTable.AddEntry("y");
-
     std::cout << "Symbol count: " << symbolTable.GetCount() << std::endl;  // Expected: 2
     std::cout << "Exists 'x'? " << (symbolTable.Exists("x") ? "true" : "false") << std::endl;  // Expected: true
     std::cout << "Exists 'z'? " << (symbolTable.Exists("z") ? "true" : "false") << std::endl;  // Expected: false
     std::cout << "Index of 'x': " << symbolTable.GetIndex("x") << std::endl;   // Expected: 0
     std::cout << "Index of 'y': " << symbolTable.GetIndex("y") << std::endl;   // Expected: 1
-
     std::cout << "Value of 'x': " << symbolTable.GetValue("x") << std::endl;  // Expected: 0
     symbolTable.SetValue("x", 42);
     std::cout << "New value of 'x': " << symbolTable.GetValue("x") << std::endl;  // Expected: 42
-
     // Uncomment these to test error handling:
     // symbolTable.AddEntry("x");
     // symbolTable.GetValue("z");
@@ -49,10 +44,7 @@ void testSymbolTable() {
 void testNodes() {
     std::cout << "\n----- Node (Parse Tree) Test -----\n";
 
-    // First, create a symbol table that all IdentifierNodes will use:
-    SymbolTableClass testSymbolTable;
-
-    // 1) Create a small, stand-alone expression tree: 10 + 20
+    // 1) Create a small expression tree: 10 + 20
     ExpressionNode* plusExpr = new PlusNode(
         new IntegerNode(10),
         new IntegerNode(20)
@@ -60,29 +52,24 @@ void testNodes() {
     // Test Evaluate (should print 30)
     std::cout << "Evaluate(10 + 20) = " << plusExpr->Evaluate() << std::endl;
 
-    // 2) Create a tiny snippet of statements:
-    //    int x;        (DeclarationStatementNode)
-    //    x = 10 + 20;  (AssignmentStatementNode)
-    //    cout << x;    (CoutStatementNode)
-
     // Declaration: int x;
     StatementNode* declStmt = new DeclarationStatementNode(
-        new IdentifierNode("x", &testSymbolTable)
+        new IdentifierNode("x", nullptr)  // Using nullptr for now if no symbol table is provided.
     );
 
-    // Assignment: x = (plusExpr2) 
+    // Assignment: x = (plusExpr)
     ExpressionNode* plusExpr2 = new PlusNode(
         new IntegerNode(10),
         new IntegerNode(20)
     );
     StatementNode* assignStmt = new AssignmentStatementNode(
-        new IdentifierNode("x", &testSymbolTable),
+        new IdentifierNode("x", nullptr),
         plusExpr2
     );
 
     // cout << x;
     StatementNode* coutStmt = new CoutStatementNode(
-        new IdentifierNode("x", &testSymbolTable)
+        new IdentifierNode("x", nullptr)
     );
 
     // 3) Put these statements into a StatementGroupNode
@@ -97,17 +84,17 @@ void testNodes() {
     // 5) Create a ProgramNode that holds this BlockNode
     ProgramNode* programNode = new ProgramNode(blockNode);
 
-    // 6) Finally, create a StartNode that holds the ProgramNode
+    // 6) Create a StartNode that holds the ProgramNode
     StartNode* startNode = new StartNode(programNode);
 
     // 7) Clean up.
-    delete plusExpr;  // This is the stand-alone expression test
-    delete startNode; // Recursively deletes all child nodes
+    delete plusExpr;  // Delete the stand-alone expression.
+    delete startNode; // Recursively deletes the entire tree.
 }
 
 int main() {
-    //testScanner();
-    //testSymbolTable();
+    // testScanner();
+    // testSymbolTable();
     testNodes();
     return 0;
 }
