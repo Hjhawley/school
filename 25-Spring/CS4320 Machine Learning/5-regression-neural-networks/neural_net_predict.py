64f628a85524140b5e41793e595790f591350592
Hayden <hjhawley7@gmail.com>
2025-03-06 16:28:38 -0700

    rewrite predict to handle multiple models at once

--- 25-Spring/CS4320 Machine Learning/5-regression-neural-networks/neural_net_predict.py
+++ 25-Spring/CS4320 Machine Learning/5-regression-neural-networks/neural_net_predict.py
@@ -6,23 +6,31 @@ import keras
 
 label = "label"
 input_filename = "test-preprocessed.csv"
-model_filename = "model.keras"
-predictions_filename = "predictions.csv"
+model_filenames = ["model-2.keras", "model-best.keras"]
+predictions_filenames = ["predictions-2.csv", "predictions-best.csv"]
 
 # Load test data
 dataframe = pd.read_csv(input_filename, index_col=0)
+
 if label in dataframe.columns:
     X = dataframe.drop(label, axis=1)
 else:
     X = dataframe
 
-# Convert to tf dataset
-y_hat = keras.models.load_model(model_filename).predict(X)
-
-# Construct a dataframe with predictions
-results_df = pd.DataFrame({
-    "Index": dataframe.index,
-    label: y_hat[:, 0]  # flatten if needed
-})
-results_df.to_csv(predictions_filename, index=False)
-print("Predictions saved to", predictions_filename)
+# Iterate over both models and generate predictions
+for model_filename, predictions_filename in zip(model_filenames, predictions_filenames):
+    # Load model
+    model = keras.models.load_model(model_filename)
+    
+    # Make predictions
+    y_hat = model.predict(X)
+    
+    # Construct a dataframe with predictions
+    results_df = pd.DataFrame({
+        "Index": dataframe.index,
+        label: y_hat[:, 0]  # Flatten if needed
+    })
+    
+    # Save predictions
+    results_df.to_csv(predictions_filename, index=False)
+    print(f"Predictions saved to {predictions_filename}")
