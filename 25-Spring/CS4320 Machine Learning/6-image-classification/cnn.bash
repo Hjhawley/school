af6ff9366c60f22194d91e35e4e0569ce28b857f
Hayden <hjhawley7@gmail.com>
2025-03-30 14:57:39 -0600

    .joblib -> .keras

--- 25-Spring/CS4320 Machine Learning/6-image-classification/cnn.bash
+++ 25-Spring/CS4320 Machine Learning/6-image-classification/cnn.bash
@@ -3,29 +3,29 @@
 model_name=c
 
 time ./cnn_classification.py cnn-fit \
-     --model-name ${model_name} --model-file ${model_name}.joblib \
+     --model-name ${model_name} --model-file ${model_name}.keras \
      --batch-number 1
 
 time ./cnn_classification.py learning-curve \
-     --model-file ${model_name}.joblib
-mv ${model_name}.joblib.learning_curve.png ${model_name}.joblib.learning_curve-a.png
+     --model-file ${model_name}.keras
+mv ${model_name}.keras.learning_curve.png ${model_name}.keras.learning_curve-a.png
 
 time ./cnn_classification.py cnn-refit \
-     --model-name ${model_name} --model-file ${model_name}.joblib \
+     --model-name ${model_name} --model-file ${model_name}.keras \
      --batch-number 2
 
 time ./cnn_classification.py learning-curve \
-     --model-file ${model_name}.joblib
-mv ${model_name}.joblib.learning_curve.png ${model_name}.joblib.learning_curve-b.png
+     --model-file ${model_name}.keras
+mv ${model_name}.keras.learning_curve.png ${model_name}.keras.learning_curve-b.png
 
 time ./cnn_classification.py cnn-refit \
-     --model-name ${model_name} --model-file ${model_name}.joblib \
+     --model-name ${model_name} --model-file ${model_name}.keras \
      --batch-number 3
 
 time ./cnn_classification.py learning-curve \
-     --model-file ${model_name}.joblib
-mv ${model_name}.joblib.learning_curve.png ${model_name}.joblib.learning_curve-c.png
+     --model-file ${model_name}.keras
+mv ${model_name}.keras.learning_curve.png ${model_name}.keras.learning_curve-c.png
 
 time ./cnn_classification.py score \
-     --model-file ${model_name}.joblib \
+     --model-file ${model_name}.keras \
      --batch-number 6
